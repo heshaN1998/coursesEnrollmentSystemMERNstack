030cc8a6783324d4bd2b14255f603229344f4a98
heshan <heshansandakelum2018@gmail.com>
2026-04-24 15:07:02 +0530

    adding material model part

---
+++ madel/courseMaterialModel.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose")
+
+const uploadDate=()=>{
+    const timeStamp=Date.now()
+    const date=new Date(timeStamp)
+    return date.toISOString().split("T")[0]
+}
+
+const courseMaterialSchema=new mongoose.Schema({
+    materialId:{type:String,required:true,unique:true},
+    fileName:{type:String,required:true},
+    materialType:{type:String,required:true},
+    material:{type:String,required:true},
+    uploadAt:{type:String,default:uploadDate()},
+    courseId:{type:String,required:true}
+})
+
+module.exports=mongoose.model("courseMaterialSchema")
