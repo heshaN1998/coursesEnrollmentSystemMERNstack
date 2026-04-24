5cd781b77ca3587cc96c59170ef6ac2398fd33f6
heshan <heshansandakelum2018@gmail.com>
2026-04-24 14:45:27 +0530

    design service section

---
+++ routes/service/courseMaterialService.js
@@ -0,0 +1,15 @@
+async function getAllMateriaks() {
+    
+}
+
+async function addCourseMaterial() {
+    
+}
+
+async function deleteCourseMaterial() {
+    
+}
+async function updateCourseMaterial() {
+    
+}
+module.exports={ getAllMateriaks,addCourseMaterial,updateCourseMaterial,deleteCourseMaterial}
